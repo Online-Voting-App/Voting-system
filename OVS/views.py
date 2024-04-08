2b1f4b42e8e90d850137b727b215ec124cfd568b
jalon360 <85844170+jalon360@users.noreply.github.com>
2024-04-07 20:22:52 -0400

    Update views.py

--- OVS/views.py
+++ OVS/views.py
@@ -1,10 +1,18 @@
 import re
+from django.shortcuts import render
 from django.utils.timezone import datetime
 from django.shortcuts import HttpResponse
 def home (request):
     return HttpResponse("Hello,django")
-def launch (request, name):
-    now= datetime.now()
-    formatted_now = now.strftime("%A,%d %B,%Y at %X")
+def launch(request, name):
+    print(request.build_absolute_uri()) #optional
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
 # Create your views here.
 
